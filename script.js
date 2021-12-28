75fa4f701149c571600e5bda1e6561b4477905f3
bhargaviRdy <bhargavisunkireddy239@gmail.com>
2021-12-28 23:39:18 +0530

    web compatible

--- script.js
+++ script.js
@@ -1,109 +1,58 @@
-let btn = document.querySelector("#search");
-let url = "http://www.omdbapi.com/?i=tt3896198&apikey=ff95f508&t=";
+const POPULAR_API = "https://api.themoviedb.org/3/discover/movie?api_key=12c713a5fea488b7e00ffd0f5315f814&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+var popularMovies = [];
+var queryResults = [];
+var res = document.querySelector("#results");
+
+function displayMovies(movies) {
+    res.innerHTML = "";
+    movies.forEach(movie => {
+        var imgSrc = "https://image.tmdb.org/t/p/w1280" + movie.poster_path;
+        res.innerHTML += ` 
+        <div class="movie">
+        <img class="poster" src=${imgSrc} alt="poster">
+
+        <span class="title">${movie.title}</span>
+        <span class="overview">${movie.overview}</span>
+        <span class="rating">${movie.vote_average}</span>
+
+    </div>
+        `
+    })
+}
+
+async function loadPopular() {
+    await fetch(POPULAR_API)
+        .then((res) => res.json())
+        .then((data) => {
+            popularMovies = data.results;
+            displayMovies(popularMovies);
+        });
+}
+loadPopular();
+
 let userInput = document.querySelector("#moviename");
 var res = document.querySelector(".results");
-btn.addEventListener("click", () => {
-    console.log(userInput.value);
-    console.log(url);
-   getMovieData(userInput.value);
-//    console.log(data);
+
+const QUERY_API = "https://api.themoviedb.org/3/search/movie?api_key=12c713a5fea488b7e00ffd0f5315f814&language=en-US&page=1&include_adult=false&query=";
+
+
+userInput.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) { getMovieData(userInput.value) }
 })
 
-async function getMovieData(movie){
-    // var d = await 
-    await fetch(url+movie)
-    .then(response => 
-        response.json()
-    )
-    .then(json => {
-        res.innerText = json.Released;
-        // output.innerText = "hello"emo
-        console.log(json);
-        // return json;
-    })
-    .catch((error) => {
-        console.log("error occured: "+error);
-    });
-    // return d;
+async function getMovieData(movie) {
+    await fetch(QUERY_API + movie)
+        .then(response =>
+            response.json()
+        )
+        .then(data => {
+            queryResults = data.results
+            displayMovies(queryResults)
+        })
+        .catch((error) => {
+            console.log("error occured: " + error);
+        });
 }
-// function findmovie(movie){
-//     console.log(movie);
-//     var request=new XMLHttpRequest();
-//     request.open('GET','http://www.omdbapi.com/?i=tt3896198&apikey=ff95f508&t='+movie,true);
-//     const fid=document.getElementById('results');
-
-//     while (fid.firstChild) {
-//         fid.firstChild.remove();
-//     }
-    
-
-//     request.onload= function(){
-//         var data = JSON.parse(this.response);
-//         if (request.status >= 200 && request.status < 400){
-
-//             const h3=document.createElement('h3');
-//             h3.setAttribute('class','col-sm-12 col-form-label');
-//             h3.textContent='You have searched for \" '+movie+" \" ";
-//             fid.appendChild(h3);
-           
-//             console.log(data);
-
-//             for(var k in data){
-//                 var v=data[k];
-//                 if(k=='Response' && v=='False'){
-//                     const r=document.createElement('label');
-//                     r.setAttribute('class','col-md-12 col-form-label ');
-//                     r.textContent=" \" "+movie+" \" " +" is not found ";
-//                     fid.appendChild(r);
-//                     break;
-//                 }
-
-//                 var d=document.createElement('div');
-//                 d.setAttribute('class','row ');
-//                 fid.appendChild(d);
-
-//                 if (k=='Poster'){
-//                     const i=document.createElement('img');
-//                     i.setAttribute('class','col-12 col-sm-12 img-thumbnail');
-//                     i.setAttribute('src',v);
-//                     i.setAttribute('alt','Poster');
-//                     i.setAttribute('style','align:center;');
-//                     // i.setAttribute('max-width','100%');
-//                     // i.setAttribute('width','200');
-//                     // i.setAttribute('height','100');
-//                     d.appendChild(i);
-//                 }
-//             }
-                   
-//             for(var k in data){
-//                 var v=data[k];
-//                 if(k!='Poster' && k!='Ratings'){
-                
-//                     const r=document.createElement('label');
-//                     r.setAttribute('class','col-md-3 col-form-label');
-//                     r.textContent=k;
-
-//                     const r2=document.createElement('label');
-//                     r2.setAttribute('class','col-md-9 col-form-label');
-//                     r2.textContent=v;
-
-//                     d.appendChild(r);
-//                     d.appendChild(r2);
-//                 }
-//             }
 
 
 
-//         }
-//         else{
-//             console.log("error");
-//         }
-//     }
-
-//     request.send();
-
-// }
-
-
-    
-     
